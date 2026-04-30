eda52bf8458ff939c5a549d7edcb5d5d36f32a1f
cuber <Pang726579@163.com>
2026-04-30 16:41:55 +0800

    Add comments to sequence fields

--- nanovllm/engine/sequence.py
+++ nanovllm/engine/sequence.py
@@ -1,33 +1,34 @@
 from copy import copy
-from enum import Enum, auto
+from enum import Enum, auto # 用于定义枚举类型和自动生成枚举值的函数
 from itertools import count
 
 from nanovllm.sampling_params import SamplingParams
 
 
-class SequenceStatus(Enum):
-    WAITING = auto()
-    RUNNING = auto()
-    FINISHED = auto()
+class SequenceStatus(Enum): # 序列状态
+    WAITING = auto() # 等待状态，等待被调度，存放在 Scheduler 的等待队列中
+    RUNNING = auto() # 运行状态，正在被 Scheduler 调度，正在被处理
+    FINISHED = auto() # 完成状态，已经完成，结果已经生成
 
 
 class Sequence:
-    block_size = 256 # 每个块能存放多少个Token的KV序列，包括所有的层和所有的KV Head
-    counter = count()
+    # 类变量，所有实例共享的属性
+    block_size = 256 # KV Cache在序列维度上的块大小，每个块能存放多少个 Token 的KV序列
+    counter = count() # 计数器，用于给每个实例化的 Sequence 生成唯一的序列ID
 
     def __init__(self, token_ids: list[int], sampling_params = SamplingParams()):
-        self.seq_id = next(Sequence.counter)
-        self.status = SequenceStatus.WAITING
-        self.token_ids = copy(token_ids)
-        self.last_token = token_ids[-1]
-        self.num_tokens = len(self.token_ids)
-        self.num_prompt_tokens = len(token_ids)
-        self.num_cached_tokens = 0    # tokens that don't need prefill
-        self.num_scheduled_tokens = 0
-        self.block_table = []
-        self.temperature = sampling_params.temperature
-        self.max_tokens = sampling_params.max_tokens
-        self.ignore_eos = sampling_params.ignore_eos
+        self.seq_id = next(Sequence.counter) # 序列ID，用于唯一标识每个实例化的 Sequence 对象
+        self.status = SequenceStatus.WAITING # 序列状态，默认是等待状态，等待被调度
+        self.token_ids = copy(token_ids) # 序列的 Token ID 列表，包括提示 Token 和新生成 Token
+        self.last_token = token_ids[-1] # 序列的 Token ID 列表的最后一个元素
+        self.num_tokens = len(self.token_ids) # 序列的 Token 数量
+        self.num_prompt_tokens = len(token_ids) # 提示 Token 的数量
+        self.num_cached_tokens = 0 # 已命中KV缓存的 Token 数量，这部分不需要重新 prefill
+        self.num_scheduled_tokens = 0 # 当前调度轮计划处理的 Token 数量
+        self.block_table = [] # 当前序列占用的 KV cache block 编号列表
+        self.temperature = sampling_params.temperature # 采样温度，控制生成随机性
+        self.max_tokens = sampling_params.max_tokens # 最多生成的 completion Token 数量
+        self.ignore_eos = sampling_params.ignore_eos # 是否忽略 EOS Token 继续生成
 
     def __len__(self):
         return self.num_tokens
@@ -80,3 +81,7 @@ class Sequence:
         else:
             self.token_ids = []
             self.last_token = last_state
+
+if __name__ == "__main__":
+    status = SequenceStatus.WAITING
+    print(status)
