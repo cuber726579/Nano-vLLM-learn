d2222554b1630910c85bdba9e1f6c10fede7e735
cuber <202420143271@mail.scut.edu.cn>
2026-05-05 13:15:17 +0800

    anno: temperture sampling

--- nanovllm/layers/sampler.py
+++ nanovllm/layers/sampler.py
@@ -6,7 +6,12 @@ class Sampler(nn.Module):
 
     @torch.compile
     def forward(self, logits: torch.Tensor, temperatures: torch.Tensor):
+        # 将 logits 转成 float32 后按每条序列的 temperature 缩放
+        # temperature 越大分布越平, 随机性越强; 越小分布越尖锐, logit 大的概率大
         logits = logits.float().div_(temperatures.unsqueeze(dim=1))
-        probs = torch.softmax(logits, dim=-1)
+
+        probs = torch.softmax(logits, dim=-1) # 将缩放后的 logits 转成概率分布
+
+        # 根据概率分布 probs 采样 token
         sample_tokens = probs.div_(torch.empty_like(probs).exponential_(1).clamp_min_(1e-10)).argmax(dim=-1)
         return sample_tokens
